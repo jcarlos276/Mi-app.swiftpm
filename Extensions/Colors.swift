e5a7c79c96ce2820ce9b2cafb18917c8e487807f
Juan Carlos Guillén <juan.cgc276@gmail.com>
2022-09-12 10:56:10 -0500

    :bento: SPIC - Add extensions to CGColor

---
+++ Extensions/Colors.swift
@@ -0,0 +1,6 @@
+import SwiftUI
+
+extension CGColor {
+    static let primary = CGColor(red: 10/255, green: 21/255, blue: 27/255, alpha: 1.0)
+    static let primaryLight = CGColor(red: 22/255, green: 60/255, blue: 60/255, alpha: 1.0)
+}
