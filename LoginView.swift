9539dd2cc4480348aef978ed2571999652c69151
Juan Carlos Guillén <juan.cgc276@gmail.com>
2022-09-23 12:17:37 -0500

    :truck: SPIC - Rename ContentView into LoginView

---
+++ LoginView.swift
@@ -0,0 +1,70 @@
+import SwiftUI
+import Stinsen
+
+struct LoginView: View {
+    // MARK: - Properties
+    @EnvironmentObject var router: MainCoordinator.Router
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    
+    var body: some View {
+        //NavigationView {
+        GeometryReader { reader in
+            ZStack {
+                background
+                VStack {
+                    header
+                        .frame(height: reader.size.height / 2)
+                    form
+                        .frame(width: reader.size.width * 0.65, height: reader.size.height / 2)
+                }
+            }
+        }
+        //}
+    }
+    
+    var background: some View {
+        LinearGradient(gradient: Gradient(colors: [Color(.primaryLight), Color(.primary),  Color(.primary)]), startPoint: .top, endPoint: .bottom)
+            .edgesIgnoringSafeArea(.all)
+    }
+    
+    var header: some View {
+        VStack {
+            Image("img_logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 110)
+        }
+    }
+    
+    var form: some View {
+        VStack(spacing: 20) {
+            TextEditor(text: $username)
+                .frame(height: 50)
+                .foregroundColor(.white)
+            TextEditor(text: $password)
+                .frame(height: 50)
+            loginButton
+            Spacer()
+        }
+        .padding(.vertical)
+    } 
+    
+    var loginButton: some View {
+        Button(action: {
+            print("Log in")
+            router.root(\.home)
+        }) {
+            Text("Log in")
+                .padding()
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity)
+                .background {
+                    RoundedRectangle(cornerRadius: 5.0)
+                        .foregroundColor(.green)
+                        .frame(width: .infinity)
+                }
+        }
+    }
+}
