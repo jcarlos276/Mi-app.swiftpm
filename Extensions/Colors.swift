56bc3956c555ea0a6c0e235e4c83deb8b537c7b7
Juan Carlos Guillén <juan.cgc276@gmail.com>
2022-10-17 11:14:22 -0500

    :necktie: SPIC - Update and add colors to CGColor extension

--- Extensions/Colors.swift
+++ Extensions/Colors.swift
@@ -1,6 +1,8 @@
 import SwiftUI
 
 extension CGColor {
-    static let primary = CGColor(red: 10/255, green: 21/255, blue: 27/255, alpha: 1.0)
+    static let almostBlack = CGColor(red: 10/255, green: 21/255, blue: 27/255, alpha: 1.0)
     static let primaryLight = CGColor(red: 22/255, green: 60/255, blue: 60/255, alpha: 1.0)
+    static let algaeGreen = CGColor(red: 33/255, green: 205/255, blue: 101/255, alpha: 1.0)
+    static let dark = CGColor(red: 26/255, green: 40/255, blue: 46/255, alpha: 1.0)
 }
