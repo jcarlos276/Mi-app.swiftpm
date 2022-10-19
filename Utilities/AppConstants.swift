c5cd4c205de0c58bafb1b240bca84c8a41cfa7c6
Juan Carlos Guillén <juan.cgc276@gmail.com>
2022-10-18 19:43:30 -0500

    :necktie: SPIC - Update constants in AppConstants file

--- Utilities/AppConstants.swift
+++ Utilities/AppConstants.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 
 enum AppConstants {
-    static let baseUrl = "api.themoviedb.org/3/" 
+    static let baseUrl = "https://api.themoviedb.org/3/"
+    static let apiKey = "2e3be257320fbcee2a0eed45b94bcfbc"
 }
