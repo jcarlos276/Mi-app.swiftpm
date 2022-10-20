31cf08bc8444a4e1caa49e4292358133106a1581
Juan Carlos Guillén <juan.cgc276@gmail.com>
2022-10-19 19:09:52 -0500

    :lipstick: SPIC - Update HomeView with ViewBuilders

--- Modules/Home/Views/HomeView.swift
+++ Modules/Home/Views/HomeView.swift
@@ -10,34 +10,44 @@ struct HomeView: View {
     ]
     
     init(viewModel: HomeViewModel = HomeViewModel()) {
+        UISegmentedControl.appearance().selectedSegmentTintColor = .darkGray
+        UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor.white], for: .normal)
         self.viewModel = viewModel
     }
     
     var body: some View {
-        GeometryReader { reader in
-            VStack {
-                Picker(selection: $viewModel.selectedCategory) { 
-                    ForEach(viewModel.categories, id: \.self) { category in
-                        Text(category.rawValue)
-                    }
-                } label: { 
-                    Text("")
-                }
-                .pickerStyle(.segmented)
-                .padding()
-                ScrollView {
-                    LazyVGrid(columns: columns) {
-                        ForEach(viewModel.tvShows, id: \.id) { tvShow in
-                            ShowCellView(show: tvShow, width: (reader.size.width / 2.0) - 10.0)
-                        }
-                    }
-                    .padding(.horizontal, 10)
-                }
-            }
-            .background(Color.init(.almostBlack))
+        VStack {
+            categoriesPicker()
+            showsList()
         }
+        .background(Color.init(.almostBlack))
         .onAppear {
             viewModel.getTvShows(category: viewModel.selectedCategory)
+        }
+    }
+    
+    @ViewBuilder func categoriesPicker() -> some View {
+        Picker(selection: $viewModel.selectedCategory) { 
+            ForEach(viewModel.categories, id: \.self) { category in
+                Text(category.rawValue)
+            }
+        } label: { 
+            Text("")
+        }
+        .pickerStyle(.segmented)
+        .padding()
+    }
+    
+    @ViewBuilder func showsList() -> some View {
+        GeometryReader { reader in
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 7) {
+                    ForEach(viewModel.tvShows, id: \.id) { tvShow in
+                        ShowCellView(show: tvShow, width: (reader.size.width / 2.0) - 12.0)
+                    }
+                }
+                .padding(.horizontal, 10)
+            }
         }
     }
 }
